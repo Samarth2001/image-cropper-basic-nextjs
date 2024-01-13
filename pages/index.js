fee258d4da68808fbe94001e6887d801550e355e
Samarth2001 <samarthsr2001@gmail.com>
2024-01-13 21:29:23 +0530

    navbar

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { useState, useEffect, useRef } from "react";
 import Cropper from "react-cropper";
 import "cropperjs/dist/cropper.css";
@@ -39,48 +40,71 @@ export default function Home() {
     fileInputRef.current.click();
   };
   return (
-    <div className="container mx-auto p-4">
-      <input
-        type="file"
-        ref={fileInputRef}
-        onChange={onFileChange}
-        accept="image/*"
-        className="hidden"
-      />
-      <button
-        onClick={handleFileButtonClick}
-        className="mb-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out"
-      >
-        Choose File
-      </button>
-      <div className="cropper-container mb-4">
-        {image && (
-          <Cropper
-            src={image}
-            style={{ height: 400, width: "100%" }}
-            initialAspectRatio={16 / 9}
-            onInitialized={(instance) => setCropper(instance)}
-            ready={() => console.log("Cropper ready")}
-          />
-        )}
-      </div>
-      <button
-        onClick={onCrop}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out"
-      >
-        Crop
-      </button>
-      <div className="cropped-images flex flex-wrap mt-4">
-        {croppedImages.map((src, index) => (
-          // eslint-disable-next-line @next/next/no-img-element
-          <img
-            key={index}
-            src={src}
-            alt={`Cropped Image ${index}`}
-            className="w-32 h-32 m-2 border p-1"
-          />
-        ))}
+    <div>
+      <nav className="bg-teal-800-700 p-4">
+        <div className="container mx-auto">
+          <h1 className="text-white text-2xl">Image Editor (In Development) </h1>
+        </div>
+      </nav>
+      <div className="container mx-auto p-40">
+        <input
+          type="file"
+          ref={fileInputRef}
+          onChange={onFileChange}
+          accept="image/*"
+          className="hidden"
+        />
+        <button
+          onClick={handleFileButtonClick}
+          className="mb-5 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out"
+        >
+          Choose File
+        </button>
+        <div className="cropper-container mb-4">
+          {image && (
+            <Cropper
+              src={image}
+              style={{ height: 400, width: "100%" }}
+              initialAspectRatio={16 / 9}
+              onInitialized={(instance) => setCropper(instance)}
+              ready={() => console.log("Cropper ready")}
+            />
+          )}
+        </div>
+        <button
+          onClick={onCrop}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out"
+        >
+          Crop
+        </button>
+        <div className="cropped-images flex flex-wrap mt-4">
+          {croppedImages.map((src, index) => (
+            // eslint-disable-next-line @next/next/no-img-element
+            <div key={index} className="m-2">
+              <img
+                src={src}
+                alt={`Cropped Image ${index}`}
+                className="w-32 h-32 border p-1"
+              />
+              <button
+                onClick={() => downloadImage(src, `cropped-image-${index}.png`)}
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded mt-2"
+              >
+                Download
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
+}
+
+function downloadImage(dataurl, filename) {
+  const link = document.createElement("a");
+  link.href = dataurl;
+  link.setAttribute("download", filename);
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
